7a21873ce77ae78e865efa68b77078247cf32138
Shweta <sanarkhede@email.arizona.edu>
2020-11-09 00:33:19 -0500

    Updated final script

--- Submissions/Team Forecast/SARS.py
+++ Submissions/Team Forecast/SARS.py
@@ -15,8 +15,8 @@ from sklearn.linear_model import LinearRegression
 
 # %%
 site = '09506000'
-start = "2000-10-08"
-end = "2020-10-24"
+start = "1989-01-01"
+end = "2020-11-07"
 
 url = "https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=" \
       + site + "&referred_module=sw&period=&begin_date=" + start + \
@@ -43,26 +43,25 @@ Comb_data = flow_weekly
 for i in range(1, 9):
     flow_weekly['tm' '%s' % (i)] = flow_weekly['flow'].shift(i)
 
-lag_cols = ['tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8']
+lag_cols = ['flow', 'tm1', 'tm2', 'tm3', 'tm4', 'tm5', 'tm6', 'tm7', 'tm8']
 # Step 2: Selecting data to use for prediction
-# LC - It looks like you are grabbing out all the rows of data so you could
-#  skip the part on line 59?
-mydata = Comb_data[(Comb_data['year'] >= 2019) &
-                   (Comb_data['month'] <= 10) &
-                   (Comb_data['month'] >= 8)][['flow', 'tm1', 'tm2',
-                                               'tm3', 'tm4', 'tm5', 'tm6',
-                                               'tm7', 'tm8']]
+
+mydata = Comb_data[(Comb_data['year'] >= 2020) &
+                   (Comb_data['month'] <= 11) &
+                   (Comb_data['month'] >= 8)][lag_cols]
 
 # %%
 # Using AR_model function to predict streamflows for 16 weeks
 # Creating empty dataframe for storing predicted flows
 predicted_flows = pd.DataFrame(columns=["Week", "Flow"])
+current_week = 11
 
 # For loop for making predictions for 16 weeks
-for i in range(16):
-    x1 = mydata[lag_cols].values
+for i in range(current_week, 16):
+
+    x1 = mydata[lag_cols[1:10]].values
     y1 = mydata[['flow']].values
-    last_week_flow = mydata.tail(1)[lag_cols]
+    last_week_flow = mydata.tail(1)[lag_cols[1:10]]
     nextweek_pred = mf.AR_Model(x1, y1, last_week_flow).round(2)
 
     mydata = mydata.append({'flow': nextweek_pred,
@@ -77,10 +76,20 @@ for i in range(16):
                             }, ignore_index=True)
 
     predicted_flows = predicted_flows.append(
-        {'Week': [i+1], 'Flow': nextweek_pred}, ignore_index=True)
+        {'Week': [i+1], 'Flow': nextweek_pred[0][0]}, ignore_index=True)
 
 print(predicted_flows)
 
+# Forecast entries for previous weeks (Week 1 to 10)
+previous_weeks_entries = pd.Series(flow_weekly.flow.tail(current_week).values)
+
+# Total 16 weeks entries to be submitted for forecast competition
+Forecast_entries = pd.DataFrame(columns=["Week", "Flow"])
+aggregate = previous_weeks_entries.append(predicted_flows.Flow)
+Forecast_entries['Flow'] = aggregate.T
+Forecast_entries['Week'] = range(1, 17)
+Forecast_entries = Forecast_entries.set_index('Week')
+print(Forecast_entries)
 # %%
 # Plotting section:
 # Previous steps: creating some additional dataset to plot.
@@ -93,10 +102,9 @@ week_hist = plotf.weekly_mean(hist_flow, 8, 22, 16)
 week_2019 = plotf.weekly_mean(data2, 8, 22, 16, 2019)
 
 # From the forecast dataframe
-week_forecast = predicted_flows['Flow'].T
+week_forecast = Forecast_entries['Flow'].T
 
 # Finally, plotting
-
 plotf.plot_3series(week_hist, week_2019, week_forecast, 2019)
 
 # %%
